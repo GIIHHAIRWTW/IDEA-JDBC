fcf1749e371d01fd7a324e521d2d86362ab6883d
Li Haotian <2878348186@qq.com>
2022-09-28 17:27:29 +0800

    Create Myclass.java

---
+++ src/pojo/Myclass.java
@@ -0,0 +1,50 @@
+package pojo;
+
+public class Myclass {
+    private int id;
+    private String name;
+    private String QQ;
+    private String Wechat;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQQ() {
+        return QQ;
+    }
+
+    public void setQQ(String QQ) {
+        this.QQ = QQ;
+    }
+
+    public String getWechat() {
+        return Wechat;
+    }
+
+    public void setWechat(String wechat) {
+        Wechat = wechat;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", QQ='" + QQ + '\'' +
+                ", Wechat='" + Wechat + '\'' +
+                '}';
+    }
+}
